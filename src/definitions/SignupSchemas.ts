6a5f4b8c7aa05876d7f96e1b1ef6db910c3e6c9d
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-06 17:25:14 +0530

    basic structure done

--- src/definitions/SignupSchemas.ts
+++ src/definitions/SignupSchemas.ts
@@ -1,4 +1,4 @@
- import { z } from "zod";
+import { z } from "zod";
 
 export const formSchema = z.object({
   name: z.string().min(1, { message: "Name is required." }),
@@ -6,4 +6,17 @@ export const formSchema = z.object({
     .string()
     .min(2, { message: "Username must be at least 2 characters." }),
   role: z.string().min(1, { message: "Role is required." }),
+});
+
+export const generalCompanySchema = z.object({
+  companyName: z.string().min(1, "Company name is required"),
+  projectDescription: z.string().min(1, "Project description is required"),
+  websiteLink: z.string().url("Must be a valid URL"),
+  chain: z.string().min(1, "Chain is required"),
+  category: z.string().min(1, "Category is required"),
+  developmentStage: z.string().min(1, "Development stage is required"),
+  teamSize: z.string().min(1, "Team size is required"),
+  usefulLinks: z.string().optional(),
+  calendlyLink: z.string().url("Must be a valid URL").optional(),
+  timezone: z.string().min(1, "Timezone is required"),
 });
