926d0ea535df8f27266b84f32dcd23e1a332ac77
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-07 16:13:29 +0530

    made adjustments for deployment

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC<NavbarProps> = ({ setStep, currentStep }) => {
       <div className="flex justify-center flex-center gap-6 px-14 md:px-5 w-1240 h-hug">
         <div className="font-medium h-hug w-hug py-1">
           <Button
-            variant={currentStep === 1 ? "ghost" : "default"}
+            variant={currentStep === 1 ? "ghost" : "ghost"}
             onClick={() => setStep(1)}
             className={currentStep === 1 ? "font-bold underline" : ""}
           >
@@ -18,7 +18,7 @@ const Navbar: React.FC<NavbarProps> = ({ setStep, currentStep }) => {
         </div>
         <div className="font-medium h-hug w-hug py-1">
           <Button
-            variant={currentStep === 2 ? "ghost" : "default"}
+            variant={currentStep === 2 ? "ghost" : "ghost"}
             onClick={() => setStep(2)}
             className={currentStep === 2 ? "font-bold underline" : ""}
           >
@@ -27,7 +27,7 @@ const Navbar: React.FC<NavbarProps> = ({ setStep, currentStep }) => {
         </div>
         <div className="font-medium h-hug w-hug py-1">
           <Button
-            variant={currentStep === 3 ? "ghost" : "default"}
+            variant={currentStep === 3 ? "ghost" : "ghost"}
             onClick={() => setStep(3)}
             className={currentStep === 3 ? "font-bold underline" : ""}
           >
@@ -36,7 +36,7 @@ const Navbar: React.FC<NavbarProps> = ({ setStep, currentStep }) => {
         </div>
         <div className="font-medium h-hug w-hug py-1">
           <Button
-            variant={currentStep === 4 ? "ghost" : "default"}
+            variant={currentStep === 4 ? "ghost" : "ghost"}
             onClick={() => setStep(4)}
             className={currentStep === 4 ? "font-bold underline" : ""}
           >
@@ -45,7 +45,7 @@ const Navbar: React.FC<NavbarProps> = ({ setStep, currentStep }) => {
         </div>
         <div className="font-medium h-hug w-hug py-1">
           <Button
-            variant={currentStep === 5 ? "ghost" : "default"}
+            variant={currentStep === 5 ? "ghost" : "ghost"}
             onClick={() => setStep(5)}
             className={currentStep === 5 ? "font-bold underline" : ""}
           >
@@ -54,7 +54,7 @@ const Navbar: React.FC<NavbarProps> = ({ setStep, currentStep }) => {
         </div>
         <div className="font-medium h-hug w-hug py-1">
           <Button
-            variant={currentStep === 6 ? "ghost" : "default"}
+            variant={currentStep === 6 ? "ghost" : "ghost"}
             onClick={() => setStep(6)}
             className={currentStep === 6 ? "font-bold underline" : ""}
           >
