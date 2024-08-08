4784ab1605edc293871ade5f2964c81967c4931b
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-08 14:39:23 +0530

    made adjustments for deployment

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -44,7 +44,7 @@ const Navbar: React.FC<NavbarProps> = ({ setStep, currentStep }) => {
   return (
     <div>
       <div className="relative">
-        <div className="flex justify-center gap-0 px-14 md:px-5 w-full">
+        <div className="flex justify-center gap-6 px-14 md:px-5 w-full">
           {[
             "Create Account",
             "General Company Information",
