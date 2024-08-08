283cbc52088ce56dac6849ded2faaf6baf899402
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-08 14:50:30 +0530

    made adjustments for deployment

--- src/components/forms/CreateAccountForm.tsx
+++ src/components/forms/CreateAccountForm.tsx
@@ -40,7 +40,7 @@ export function CreateAccountForm() {
   return (
     <div className="main-div flex flex-col min-h-screen px-20">
       <div className="flex flex-row py-10 px-5 flex-grow">
-        <div className="w-260 h-168 font-normal text-justify text-base text-gray-500   py-[59px]">
+        <div className="w-260 h-168 font-normal text-justify text-base text-gray-500 px-[100px]   py-[59px]">
           Let us know a bit more about you, and
           <br /> please provide some social links, so we <br /> can run a small
           background check on <br /> you. <br /> <br />
