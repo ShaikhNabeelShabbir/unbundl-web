550f58df1db4291e25a6dd57dd96d2107b500ab1
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-13 20:27:20 +0530

    formatted legal room

--- src/pages/startup/leagal-room.tsx
+++ src/pages/startup/leagal-room.tsx
@@ -11,58 +11,137 @@ import {
 
 const LegalRoom: React.FC = () => {
   return (
-    <div className="flex">
+    <div className="flex ">
       <div className="flex-1 py-[97px]">
         <div className="flex items-center justify-between">
           <p className="font-semibold text-[20px]">Legal Room</p>
-          <Button className="w-[178px] h-[42px] mr-[30px]">Publish</Button>
+          <Button
+            variant="secondary"
+            className="w-[178px] h-[42px] mr-[15px] ml-auto bg-white border"
+          >
+            Connect with a lawyer{" "}
+          </Button>
+          <Button className="w-[190px] h-[46px] mr-[30px]">
+            Request to add a template{" "}
+          </Button>
         </div>
         <div className="flex items-center justify-between mt-[21px]">
-          <div className="flex w-[950px] h-[46px] border border-red-900 rounded-[4px]">
+          <div className="flex w-[1150px] h-[46px] border rounded-[4px]">
             <input
               type="text"
               className="w-full px-4 py-2 outline-none"
               placeholder="Search..."
             />
           </div>
-          <Button className="w-[190px] h-[46px] mr-[30px]">
-            Advanced Search
-          </Button>
         </div>
         <div className="mt-[30px] mb-[20px] font-medium text-base">
-          Best Crypto Events in Dubai{" "}
+          -----------NAVBAR----------
         </div>
         <div className="flex flex-col space-y-[20px]">
           <div className="flex flex-row space-x-[20px]">
-            <Card className="w-[370px] h-[200px]">
+            <Card className="w-[370px] h-[200px] flex flex-col justify-between">
               <CardHeader>
                 <CardTitle className="w-[330px] h-[19px]">
                   SAFT Contract Template{" "}
                 </CardTitle>
               </CardHeader>
-              <CardContent>
-                <p className="mt-[10px]">
+              <CardContent className="flex-grow">
+                <p className="font-normal text-sm">
                   A simple agreement for future equity (SAFT) is a financing
                   contract that may be used by a startup company to raise
                   capital in its seed financing rounds.
                 </p>
               </CardContent>
               <CardFooter>
-                <Button>Download</Button>
+                <Button className="w-full">Download</Button>
+              </CardFooter>
+            </Card>
+            <Card className="w-[370px] h-[200px] flex flex-col justify-between">
+              <CardHeader>
+                <CardTitle className="w-[330px] h-[19px]">
+                  SAFT Contract Template{" "}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex-grow">
+                <p className="font-normal text-sm">
+                  A simple agreement for future equity (SAFT) is a financing
+                  contract that may be used by a startup company to raise
+                  capital in its seed financing rounds.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button className="w-full">Download</Button>
+              </CardFooter>
+            </Card>
+            <Card className="w-[370px] h-[200px] flex flex-col justify-between">
+              <CardHeader>
+                <CardTitle className="w-[330px] h-[19px]">
+                  SAFT Contract Template{" "}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex-grow">
+                <p className="font-normal text-sm">
+                  A simple agreement for future equity (SAFT) is a financing
+                  contract that may be used by a startup company to raise
+                  capital in its seed financing rounds.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button className="w-full">Download</Button>
               </CardFooter>
             </Card>
           </div>
           <div className="flex flex-row space-x-[20px]">
-            <Card className="w-[372px] h-[142px]">
+            <Card className="w-[370px] h-[200px] flex flex-col justify-between">
               <CardHeader>
-                <CardDescription>Nov. 27 2022</CardDescription>
-                <CardTitle>
-                  International Crypto Expo Dubai 2023(iCryptoExpo)
+                <CardTitle className="w-[330px] h-[19px]">
+                  SAFT Contract Template{" "}
                 </CardTitle>
               </CardHeader>
-              <CardContent>
-                <p>Jumeirah Emirates Towers • Dubai</p>
+              <CardContent className="flex-grow">
+                <p className="font-normal text-sm">
+                  A simple agreement for future equity (SAFT) is a financing
+                  contract that may be used by a startup company to raise
+                  capital in its seed financing rounds.
+                </p>
               </CardContent>
+              <CardFooter>
+                <Button className="w-full">Download</Button>
+              </CardFooter>
+            </Card>
+            <Card className="w-[370px] h-[200px] flex flex-col justify-between">
+              <CardHeader>
+                <CardTitle className="w-[330px] h-[19px]">
+                  SAFT Contract Template{" "}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex-grow">
+                <p className="font-normal text-sm">
+                  A simple agreement for future equity (SAFT) is a financing
+                  contract that may be used by a startup company to raise
+                  capital in its seed financing rounds.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button className="w-full">Download</Button>
+              </CardFooter>
+            </Card>
+            <Card className="w-[370px] h-[200px] flex flex-col justify-between">
+              <CardHeader>
+                <CardTitle className="w-[330px] h-[19px]">
+                  SAFT Contract Template{" "}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex-grow">
+                <p className="font-normal text-sm">
+                  A simple agreement for future equity (SAFT) is a financing
+                  contract that may be used by a startup company to raise
+                  capital in its seed financing rounds.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button className="w-full">Download</Button>
+              </CardFooter>
             </Card>
           </div>
         </div>
