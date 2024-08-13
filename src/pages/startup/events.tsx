97f9c8af7bd4024eeff258e9d2f9986dc9ddce3a
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-13 17:09:15 +0530

    adjustment for deployment

--- src/pages/startup/events.tsx
+++ src/pages/startup/events.tsx
@@ -7,22 +7,41 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const Events: React.FC = () => {
   return (
-    <div className="flex">
+    <div className="flex ml-[30px]">
       <div className="flex-1 py-[97px]">
         <div className="flex items-center justify-between">
           <p className="font-semibold text-[20px]">Events</p>
-          <Button className="w-[178px] h-[42px] mr-[30px]">Publish</Button>
+          <Button className="w-[178px] h-[42px] mr-[30px]">Add Event</Button>
         </div>
         <div className="flex items-center justify-between mt-[21px]">
-          <div className="flex w-[950px] h-[46px] border border-red-900 rounded-[4px]">
+          <div className="flex w-[770px] h-[46px] border">
             <input
               type="text"
               className="w-full px-4 py-2 outline-none"
               placeholder="Search..."
             />
+          </div>
+          <div className="w-[170px] h-[45px]">
+            <Select>
+              <SelectTrigger className="w-[170px] h-[45px]">
+                <SelectValue placeholder="Dubai" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Dubai">Dubai</SelectItem>
+                <SelectItem value="Mumbai">Mumbai</SelectItem>
+                <SelectItem value="USA">USA</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
           <Button className="w-[190px] h-[46px] mr-[30px]">
             Advanced Search
@@ -67,7 +86,7 @@ const Events: React.FC = () => {
               </CardContent>
             </Card>
           </div>
-          <div className="flex flex-row space-x-[20px]">
+          <div className="flex flex-row space-x-[20px] mr-[30px]">
             <Card className="w-[372px] h-[142px]">
               <CardHeader>
                 <CardDescription>Nov. 27 2022</CardDescription>
@@ -101,6 +120,10 @@ const Events: React.FC = () => {
                 <p>Jumeirah Emirates Towers • Dubai</p>
               </CardContent>
             </Card>
+          </div>
+          <div className="flex flex-row mr-[30px]">
+            {" "}
+            <Button className="ml-auto">Sponsorship Strategy</Button>
           </div>
         </div>
       </div>
--- src/pages/startup/research-library.tsx
+++ src/pages/startup/research-library.tsx
@@ -11,14 +11,14 @@ import {
 
 const ResearchLibrary: React.FC = () => {
   return (
-    <div className="flex">
+    <div className="flex ml-[30px]">
       <div className="flex-1 py-[97px]">
         <div className="flex items-center justify-between">
           <p className="font-semibold text-[20px]">Research Library</p>
           <Button className="w-[178px] h-[42px] mr-[30px]">Publish</Button>
         </div>
         <div className="flex items-center justify-between mt-[21px]">
-          <div className="flex w-[950px] h-[46px] border border-red-900 rounded-[4px]">
+          <div className="flex w-[950px] h-[46px] border rounded-[4px]">
             <input
               type="text"
               className="w-full px-4 py-2 outline-none"
