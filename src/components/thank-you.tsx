e628ff651a4a372221f10b9ceccb7365e8318713
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-19 11:09:15 +0530

    changes updated

--- src/components/thank-you.tsx
+++ src/components/thank-you.tsx
@@ -1,27 +1,17 @@
-
 export function Thankyou() {
-  // Define the submit handler
   return (
-    <div>
-      <div className="main-div">
-        <div className=" py-[40px] ">
-          <div className="justify-center ">
-            <div className="flex px-[440px] justify-center">
-              <div>
-                Thank you for creating an account!
-                <br />
-                <br />
-                <p className="font-medium ">
-                  Your information is under review. We select startups very
-                  carefully, as we want to keep the quality top notch on
-                  Unbundl. You are going to get a confirmation email in the next
-                  24 hours. We might ask you to jump on a call with us, before
-                  approving your profile.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="main-div mx-[379px] mt-[173px]">
+      <div className="w-[682px] h-[311px] ">
+        <p className="h-[39px] font-semibold text-[32px] text-center">
+          Thank you for creating an account!
+        </p>
+        <p className="mt-[20px] text-center font-medium text-[24px]">
+          Your information is under review. We select startups very carefully,
+          as we want to keep the quality top notch on Unbundl.
+          <br /> You are going to get a confirmation email in the next 24 hours.
+          We might ask you to jump on a call with us, before approving your
+          profile.
+        </p>
       </div>
     </div>
   );
