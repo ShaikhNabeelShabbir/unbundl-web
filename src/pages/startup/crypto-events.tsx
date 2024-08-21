5ca0cfe5cb90b202e379074d0fae11f63dfd457f
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-21 19:10:51 +0530

    changes updated

--- src/pages/startup/crypto-events.tsx
+++ src/pages/startup/crypto-events.tsx
@@ -18,14 +18,14 @@ import { Search } from "lucide-react";
 
 const CryptoEvents: React.FC = () => {
   return (
-    <div className="flex ml-[30px]">
+    <div className="flex flex-wrap m-8">
       <div className="flex-1 py-[97px]">
         <div className="flex items-center justify-between">
           <p className="font-semibold text-5">Crypto Events</p>
-          <Button className="w-[178px] h-[42px] mr-[30px]">Add Event</Button>
+          <Button className="w-[178px] h-11 mr-8">Add Event</Button>
         </div>
-        <div className="flex items-center justify-between mt-[21px]">
-          <div className="flex items-center w-[770px] h-[46px] border rounded-1">
+        <div className="flex items-center justify-between mt-5">
+          <div className="flex items-center w-full h-12 border rounded-1">
             <Search className="ml-4 text-gray-500" />
             <input
               type="text"
@@ -33,9 +33,9 @@ const CryptoEvents: React.FC = () => {
               placeholder="Search..."
             />
           </div>
-          <div className="w-[170px] h-[45px]">
+          <div className="w-[170px] h-12">
             <Select>
-              <SelectTrigger className="w-[170px] h-[45px]">
+              <SelectTrigger className="w-[170px] h-12 ml-3">
                 <SelectValue placeholder="Dubai" />
               </SelectTrigger>
               <SelectContent>
@@ -45,16 +45,14 @@ const CryptoEvents: React.FC = () => {
               </SelectContent>
             </Select>
           </div>
-          <Button className="w-[190px] h-[46px] mr-[30px]">
-            Advanced Search
-          </Button>
+          <Button className="w-[190px] h-12 mr-8 ml-3">Advanced Search</Button>
         </div>
-        <div className="mt-[30px] mb-5 font-medium text-base mr-[30px]">
+        <div className="mt-8 mb-5 font-medium text-base mr-8">
           Best Crypto Events in Dubai{" "}
         </div>
-        <div className="flex flex-col space-y-5 mr-[30px]">
-          <div className="flex flex-row space-x-5">
-            <Card className="w-[372px] h-[150px]">
+        <div className="flex flex-col space-y-5 mr-8">
+          <div className="flex flex-row space-x-5 w-full">
+            <Card className="w-full h-[150px]">
               <CardHeader>
                 <CardDescription>Nov. 27 2022</CardDescription>
                 <CardTitle className="font-semibold text-base">
@@ -65,7 +63,7 @@ const CryptoEvents: React.FC = () => {
                 <p>Jumeirah Emirates Towers • Dubai</p>
               </CardContent>
             </Card>
-            <Card className="w-[372px] h-[150px]">
+            <Card className="w-full h-[150px]">
               <CardHeader>
                 <CardDescription>Nov. 27 2022</CardDescription>
                 <CardTitle className="font-semibold text-base">
@@ -76,42 +74,7 @@ const CryptoEvents: React.FC = () => {
                 <p>Jumeirah Emirates Towers • Dubai</p>
               </CardContent>
             </Card>
-            <Card className="w-[372px] h-[150px]">
-              <CardHeader>
-                <CardDescription>Nov. 27 2022</CardDescription>
-                <CardTitle className="font-semibold text-base">
-                  International Crypto Expo Dubai 2023(iCryptoExpo)
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>Jumeirah Emirates Towers • Dubai</p>
-              </CardContent>
-            </Card>
-          </div>
-          <div className="flex flex-row space-x-5 mr-[30px]">
-            <Card className="w-[372px] h-[150px]">
-              <CardHeader>
-                <CardDescription>Nov. 27 2022</CardDescription>
-                <CardTitle className="font-semibold text-base">
-                  International Crypto Expo Dubai 2023(iCryptoExpo)
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>Jumeirah Emirates Towers • Dubai</p>
-              </CardContent>
-            </Card>
-            <Card className="w-[372px] h-[150px]">
-              <CardHeader>
-                <CardDescription>Nov. 27 2022</CardDescription>
-                <CardTitle className="font-semibold text-base">
-                  International Crypto Expo Dubai 2023(iCryptoExpo)
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>Jumeirah Emirates Towers • Dubai</p>
-              </CardContent>
-            </Card>
-            <Card className="w-[372px] h-[150px]">
+            <Card className="w-full h-[150px]">
               <CardHeader>
                 <CardDescription>Nov. 27 2022</CardDescription>
                 <CardTitle className="font-semibold text-base">
@@ -123,7 +86,42 @@ const CryptoEvents: React.FC = () => {
               </CardContent>
             </Card>
           </div>
-          <div className="flex flex-row mr-[30px]">
+          <div className="flex flex-row space-x-5 mr-8 w-full">
+            <Card className="w-full h-[150px]">
+              <CardHeader>
+                <CardDescription>Nov. 27 2022</CardDescription>
+                <CardTitle className="font-semibold text-base">
+                  International Crypto Expo Dubai 2023(iCryptoExpo)
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Jumeirah Emirates Towers • Dubai</p>
+              </CardContent>
+            </Card>
+            <Card className="w-full h-[150px]">
+              <CardHeader>
+                <CardDescription>Nov. 27 2022</CardDescription>
+                <CardTitle className="font-semibold text-base">
+                  International Crypto Expo Dubai 2023(iCryptoExpo)
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Jumeirah Emirates Towers • Dubai</p>
+              </CardContent>
+            </Card>
+            <Card className="w-full h-[150px]">
+              <CardHeader>
+                <CardDescription>Nov. 27 2022</CardDescription>
+                <CardTitle className="font-semibold text-base">
+                  International Crypto Expo Dubai 2023(iCryptoExpo)
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Jumeirah Emirates Towers • Dubai</p>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="flex flex-row">
             {" "}
             <Button className="ml-auto">Sponsorship Strategy</Button>
           </div>
