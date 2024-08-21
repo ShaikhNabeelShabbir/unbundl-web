49ec61580a54df0b43364b67e3d238a34660c2db
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-21 19:53:09 +0530

    changes updated

--- src/pages/startup/grants.tsx
+++ src/pages/startup/grants.tsx
@@ -26,13 +26,13 @@ import ApplyGrant from "./apply-grant";
 
 const Grants: React.FC = () => {
   return (
-    <div className="flex ml-[30px]">
+    <div className="flex flex-wrap m-8">
       <div className="flex-1 py-[97px]">
         <div className="flex items-center justify-between">
           <p className="font-semibold text-5">Grants</p>
         </div>
-        <div className="flex items-center justify-between mt-[21px]">
-          <div className="flex items-center w-[770px] h-[46px] border rounded-1">
+        <div className="flex items-center justify-between mt-5">
+          <div className="flex items-center w-full h-12 border rounded-1">
             <Search className="ml-4 text-gray-500" />
             <input
               type="text"
@@ -40,9 +40,9 @@ const Grants: React.FC = () => {
               placeholder="Search..."
             />
           </div>
-          <div className="w-[170px] h-[45px]">
+          <div className="w-[170px] h-11">
             <Select>
-              <SelectTrigger className="w-[170px] h-[45px]">
+              <SelectTrigger className="w-[170px] h-12 ml-3">
                 <SelectValue placeholder="Dubai" />
               </SelectTrigger>
               <SelectContent>
@@ -52,16 +52,14 @@ const Grants: React.FC = () => {
               </SelectContent>
             </Select>
           </div>
-          <Button className="w-[190px] h-[46px] mr-[30px]">
-            Advanced Search
-          </Button>
+          <Button className="w-[190px] h-12 ml-3">Advanced Search</Button>
         </div>
-        <div className="mt-[30px] mb-5 font-medium text-base mr-[30px]">
+        <div className="mt-8 mb-5 font-medium text-base mr-8">
           Best Grants for your project{" "}
         </div>
-        <div className="flex flex-col space-y-5 mr-[30px]">
-          <div className="flex flex-row space-x-5">
-            <Card className="w-[372px] h-[184px]">
+        <div className="flex flex-col space-y-5">
+          <div className="flex flex-row w-full space-x-5">
+            <Card className="w-full h-[184px]">
               <CardHeader>
                 <CardDescription>Near foundation</CardDescription>
                 <CardTitle className="font-semibold text-base">
@@ -72,10 +70,7 @@ const Grants: React.FC = () => {
               <CardContent>
                 <Dialog>
                   <DialogTrigger asChild>
-                    <Button
-                      type="button"
-                      className="h-[42px] w-[330px] text-sm px-[15px]"
-                    >
+                    <Button type="button" className="h-11 w-full text-sm px-4">
                       Apply For Grant{" "}
                     </Button>
                   </DialogTrigger>
@@ -90,7 +85,7 @@ const Grants: React.FC = () => {
                 </Dialog>{" "}
               </CardContent>
             </Card>
-            <Card className="w-[372px] h-[184px]">
+            <Card className="w-full h-[184px]">
               <CardHeader>
                 <CardDescription>Near foundation</CardDescription>
                 <CardTitle className="font-semibold text-base">
@@ -101,10 +96,7 @@ const Grants: React.FC = () => {
               <CardContent>
                 <Dialog>
                   <DialogTrigger asChild>
-                    <Button
-                      type="button"
-                      className="h-[42px] w-[330px] text-sm px-[15px]"
-                    >
+                    <Button type="button" className="h-11 w-full text-sm px-4">
                       Apply For Grant{" "}
                     </Button>
                   </DialogTrigger>
@@ -119,7 +111,7 @@ const Grants: React.FC = () => {
                 </Dialog>{" "}
               </CardContent>
             </Card>
-            <Card className="w-[372px] h-[184px]">
+            <Card className="w-full h-[184px]">
               <CardHeader>
                 <CardDescription>Near foundation</CardDescription>
                 <CardTitle className="font-semibold text-base">
@@ -130,99 +122,7 @@ const Grants: React.FC = () => {
               <CardContent>
                 <Dialog>
                   <DialogTrigger asChild>
-                    <Button
-                      type="button"
-                      className="h-[42px] w-[330px] text-sm px-[15px]"
-                    >
-                      Apply For Grant{" "}
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[600px]">
-                    <DialogHeader>
-                      <DialogTitle className="font-semibold text-xl">
-                        Apply for Grant Name Grant{" "}
-                      </DialogTitle>
-                    </DialogHeader>
-                    <ApplyGrant />
-                  </DialogContent>
-                </Dialog>{" "}
-              </CardContent>
-            </Card>
-          </div>
-          <div className="flex flex-row space-x-5 mr-[30px]">
-            <Card className="w-[372px] h-[184px]">
-              <CardHeader>
-                <CardDescription>Near foundation</CardDescription>
-                <CardTitle className="font-semibold text-base">
-                  NEAR GRANT PROGRAMM{" "}
-                </CardTitle>
-                <CardDescription>$15k-$500k</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      type="button"
-                      className="h-[42px] w-[330px] text-sm px-[15px]"
-                    >
-                      Apply For Grant{" "}
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[600px]">
-                    <DialogHeader>
-                      <DialogTitle className="font-semibold text-xl">
-                        Apply for Grant Name Grant{" "}
-                      </DialogTitle>
-                    </DialogHeader>
-                    <ApplyGrant />
-                  </DialogContent>
-                </Dialog>{" "}
-              </CardContent>
-            </Card>
-            <Card className="w-[372px] h-[184px]">
-              <CardHeader>
-                <CardDescription>Near foundation</CardDescription>
-                <CardTitle className="font-semibold text-base">
-                  NEAR GRANT PROGRAMM{" "}
-                </CardTitle>
-                <CardDescription>$15k-$500k</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      type="button"
-                      className="h-[42px] w-[330px] text-sm px-[15px]"
-                    >
-                      Apply For Grant{" "}
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[600px]">
-                    <DialogHeader>
-                      <DialogTitle className="font-semibold text-xl">
-                        Apply for Grant Name Grant{" "}
-                      </DialogTitle>
-                    </DialogHeader>
-                    <ApplyGrant />
-                  </DialogContent>
-                </Dialog>{" "}
-              </CardContent>
-            </Card>
-            <Card className="w-[372px] h-[184px]">
-              <CardHeader>
-                <CardDescription>Near foundation</CardDescription>
-                <CardTitle className="font-semibold text-base">
-                  NEAR GRANT PROGRAMM{" "}
-                </CardTitle>
-                <CardDescription>$15k-$500k</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      type="button"
-                      className="h-[42px] w-[330px] text-sm px-[15px]"
-                    >
+                    <Button type="button" className="h-10 w-full text-sm px-4">
                       Apply For Grant{" "}
                     </Button>
                   </DialogTrigger>
@@ -238,7 +138,87 @@ const Grants: React.FC = () => {
               </CardContent>
             </Card>
           </div>
-          <div className="flex flex-row mr-[30px]"> </div>
+          <div className="flex flex-row space-x-5 w-full mr-8">
+            <Card className="w-full h-[184px]">
+              <CardHeader>
+                <CardDescription>Near foundation</CardDescription>
+                <CardTitle className="font-semibold text-base">
+                  NEAR GRANT PROGRAMM{" "}
+                </CardTitle>
+                <CardDescription>$15k-$500k</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button type="button" className="h-10 w-full text-sm px-4">
+                      Apply For Grant{" "}
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[600px]">
+                    <DialogHeader>
+                      <DialogTitle className="font-semibold text-xl">
+                        Apply for Grant Name Grant{" "}
+                      </DialogTitle>
+                    </DialogHeader>
+                    <ApplyGrant />
+                  </DialogContent>
+                </Dialog>{" "}
+              </CardContent>
+            </Card>
+            <Card className="w-full h-[184px]">
+              <CardHeader>
+                <CardDescription>Near foundation</CardDescription>
+                <CardTitle className="font-semibold text-base">
+                  NEAR GRANT PROGRAMM{" "}
+                </CardTitle>
+                <CardDescription>$15k-$500k</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button type="button" className="h-10 w-full text-sm px-4">
+                      Apply For Grant{" "}
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[600px]">
+                    <DialogHeader>
+                      <DialogTitle className="font-semibold text-xl">
+                        Apply for Grant Name Grant{" "}
+                      </DialogTitle>
+                    </DialogHeader>
+                    <ApplyGrant />
+                  </DialogContent>
+                </Dialog>{" "}
+              </CardContent>
+            </Card>
+            <Card className="w-full h-[184px]">
+              <CardHeader>
+                <CardDescription>Near foundation</CardDescription>
+                <CardTitle className="font-semibold text-base">
+                  NEAR GRANT PROGRAMM{" "}
+                </CardTitle>
+                <CardDescription>$15k-$500k</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button type="button" className="h-10 w-full text-sm px-4">
+                      Apply For Grant{" "}
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[600px]">
+                    <DialogHeader>
+                      <DialogTitle className="font-semibold text-xl">
+                        Apply for Grant Name Grant{" "}
+                      </DialogTitle>
+                    </DialogHeader>
+                    <ApplyGrant />
+                  </DialogContent>
+                </Dialog>{" "}
+              </CardContent>
+            </Card>
+          </div>
+          <div className="flex flex-row mr-8"> </div>
         </div>
       </div>
     </div>
