c9d2606bde42992822626fa61ad833729732ce3b
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-27 17:12:13 +0530

    reach out to investors updated

--- src/pages/startup/expand-your-network.tsx
+++ src/pages/startup/expand-your-network.tsx
@@ -1,115 +1,52 @@
-import { Button } from "@/components/ui/button";
+import { ExpandYourNetworkDataTable } from "@/components/expandyournetwork/expand-your-network-data-table";
+import { useEffect, useState } from "react";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { Search } from "lucide-react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import RequestIntroduction from "./request-introduction";
+  columns,
+  ExpandYourNetworkInfo,
+} from "@/components/expandyournetwork/expand-your-network-columns";
 
-const expand_you_network = [
-  {
-    nameOrganiztion: "Polychain Capital",
-    category: "VC",
-    location: "Dubai,UAE",
-    mutualConnections: "3",
-    levelOfConnection: "3",
-  },
-  {
-    nameOrganiztion: "Sequoia Capital",
-    category: "VC",
-    location: "San Francisco, USA",
-    mutualConnections: "5",
-    levelOfConnection: "2",
-  },
-  {
-    nameOrganiztion: "Andreessen Horowitz",
-    category: "VC",
-    location: "Menlo Park, USA",
-    mutualConnections: "4",
-    levelOfConnection: "1",
-  },
-];
+async function fetchData(): Promise<ExpandYourNetworkInfo[]> {
+  return [
+    {
+      nameOrganiztion: "Polychain Capital",
+      category: "VC",
+      location: "Dubai,UAE",
+      mutualConnections: 3,
+      levelOfConnection: 3,
+    },
+    {
+      nameOrganiztion: "Sequoia Capital",
+      category: "VC",
+      location: "San Francisco, USA",
+      mutualConnections: 5,
+      levelOfConnection: 2,
+    },
+    {
+      nameOrganiztion: "Andreessen Horowitz",
+      category: "VC",
+      location: "Menlo Park, USA",
+      mutualConnections: 4,
+      levelOfConnection: 1,
+    },
+  ];
+}
 
 const ExpandYourNetwork = () => {
+  const [data, setData] = useState<ExpandYourNetworkInfo[]>([]);
+
+  useEffect(() => {
+    // Fetch data on component mount
+    const loadData = async () => {
+      const investments = await fetchData();
+      setData(investments);
+    };
+
+    loadData();
+  }, []);
   return (
     <div className="w-full h-fit px-5">
-      <div className="flex flex-row space-x-3 mb-4">
-        <div className="flex items-center w-full h-[46px] border rounded-1 ml-3">
-          <Search className="ml-4 text-gray-500" />
-          <input
-            type="text"
-            className="w-full px-4 py-2 outline-black bg-transparent"
-            placeholder="Search..."
-          />
-        </div>
-        <Select>
-          <SelectTrigger className="w-[170px] h-11 bg-transparent">
-            <SelectValue placeholder="Choose Category" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="Founder">Founder</SelectItem>
-            <SelectItem value="CEO">CEO</SelectItem>
-            <SelectItem value="CTO">CTO</SelectItem>
-            <SelectItem value="COO">COO</SelectItem>
-          </SelectContent>
-        </Select>
-        <Button className="w-[170px] h-11">Filters</Button>
-      </div>
-      <Table className="border w-full h-fit">
-        <TableHeader>
-          <TableRow>
-            <TableHead>Organization Name</TableHead>
-            <TableHead>Category</TableHead>
-            <TableHead>Location</TableHead>
-            <TableHead>Mutual Connections</TableHead>
-            <TableHead>Level of Connection</TableHead>
-            <TableHead>Action button</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {expand_you_network.map((organization, index) => (
-            <TableRow key={index}>
-              <TableCell className="font-semibold text-md">
-                {organization.nameOrganiztion}
-              </TableCell>
-              <TableCell className="font-semibold text-base">
-                {organization.category}
-              </TableCell>
-              <TableCell className="font-semibold text-base">
-                {organization.location}
-              </TableCell>
-              <TableCell className="font-semibold text-base">
-                {organization.mutualConnections}
-              </TableCell>
-              <TableCell className="font-semibold text-base">
-                {organization.levelOfConnection}
-              </TableCell>
-              <TableCell className="font-semibold text-base">
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <u className="cursor-pointer">Request Introduction</u>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[600px]">
-                    <RequestIntroduction />
-                  </DialogContent>
-                </Dialog>{" "}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <div className="flex flex-row space-x-3 mb-4"></div>
+      <ExpandYourNetworkDataTable columns={columns} data={data} />
     </div>
   );
 };
