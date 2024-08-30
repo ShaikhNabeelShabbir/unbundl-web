3ef7c10ebf87c707c1e1e928ad04d173afa43d09
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-30 15:54:00 +0530

    changes updated

--- src/components/forms/create-gigs.tsx
+++ src/components/forms/create-gigs.tsx
@@ -4,34 +4,28 @@ import CreateNewService from "../create-new-service";
 
 export function CreateGigs() {
   return (
-    <div className="main-div flex flex-col px-20 flex-wrap w-full items-center ">
-      <div className="main-div">
-        <div className="py-[40px]">
-          <div className="justify-center">
-            <div className="flex px-[440px] justify-center">
-              <div className="text-base text-black/50">
-                Add services that you provide, like “Token Design”, or “I will
-                give you a 30-minute consultation on tokenomics”
-                <br />
-                <br />
-                <p className="font-medium text-base text-black">
-                  Create Services
-                </p>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      type="button"
-                      className="h-11 w-[136px] mt-4 text-[12px] font-normal px-5 py-4"
-                    >
-                      Add your service
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent className="sm:max-w-[600px]">
-                    <CreateNewService />
-                  </DialogContent>
-                </Dialog>
-              </div>
-            </div>
+    <div>
+      <div className="main-div flex flex-col px-4 md:px-20 w-full items-center">
+        <div className="flex flex-col md:flex-row py-10 md:px-5 flex-grow">
+          <div className="justify-center w-full md:w-[560px] py-2">
+            Add services that you provide, like “Token Design”, or “I will give
+            you a 30-minute consultation on tokenomics”
+            <br />
+            <br />
+            <p className="font-medium text-base text-black">Create Services</p>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  type="button"
+                  className="h-11 w-[136px] mt-4 text-[12px] font-normal px-5 py-4"
+                >
+                  Add your service
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[600px]">
+                <CreateNewService />
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
       </div>
