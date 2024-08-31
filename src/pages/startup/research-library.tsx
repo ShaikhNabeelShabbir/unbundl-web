558431f8ddbd9bbe12f5fe4075ef79e6c9318d8a
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-31 14:55:41 +0530

    updated the founder dashboard research library section

--- src/pages/startup/research-library.tsx
+++ src/pages/startup/research-library.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -15,9 +15,27 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Search } from "lucide-react";
+import { ChevronDown, ChevronUp, Search } from "lucide-react";
 
 const ResearchLibrary: React.FC = () => {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+
+  const handleIncrement = (
+    setCount: React.Dispatch<React.SetStateAction<number>>,
+  ) => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = (
+    count: number,
+    setCount: React.Dispatch<React.SetStateAction<number>>,
+  ) => {
+    if (count > 0) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
   return (
     <div className="flex flex-col p-4 sm:p-8">
       <div className="flex flex-col sm:flex-row items-center justify-between mb-6">
@@ -86,73 +104,122 @@ const ResearchLibrary: React.FC = () => {
       </div>
       <div className="flex flex-col space-y-5">
         <div className="flex flex-col space-y-5 sm:flex-row sm:space-x-5 sm:space-y-0">
-          <Card className="w-full h-full">
+          <Card className="w-full h-[260px]">
             <CardHeader>
-              <CardTitle>Figma</CardTitle>
-              <CardDescription>Published by Ramsy</CardDescription>
+              <CardDescription>Nov. 27 2022</CardDescription>
+              <CardTitle className="flex items-center space-x-2">
+                <span className="w-fit h-[19px]">
+                  How is Web3 going to transform the dating industry
+                </span>
+                <div className="flex items-center space-x-1">
+                  <ChevronUp
+                    className="cursor-pointer"
+                    onClick={() => handleIncrement(setCount1)}
+                  />
+                  <span>{count1}</span>
+                  <ChevronDown
+                    className="cursor-pointer"
+                    onClick={() => handleDecrement(count1, setCount1)}
+                  />
+                </div>
+              </CardTitle>
+              <CardDescription className="pt-3">
+                Published by Ramsy
+              </CardDescription>
             </CardHeader>
-            <CardContent className="font-medium text-sm">
-              Figma is a collaborative web application for interface design,
-              with additional offline features enabled by desktop applications
-              for macOS and Windows.
+            <CardContent className="font-medium text-sm space-x-3 w-full">
+              <Button className="p-3 bg-black/25">Blockchain</Button>
+              <Button className="p-3  bg-black/25">Web3</Button>
+              <Button className="p-3  bg-black/25">Dating</Button>
+              <Button className="p-3  bg-black/25">Marketing</Button>
             </CardContent>
-            <CardFooter className="font-medium text-sm flex flex-wrap gap-2">
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                UI/UX Design
+            <CardFooter className="font-medium text-sm space-x-3">
+              <Button className="p-3 w-full h-11 py-4 px-[55px]  bg-black/50">
+                View
               </Button>
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                Web Design
-              </Button>
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                Mobile Design
+              <Button className="p-3 w-full h-11 py-4 px-[55px]">
+                Download
               </Button>
             </CardFooter>
           </Card>
           {/* Repeat the Card for other research papers */}
-          <Card className="w-full h-full">
+          <Card className="w-full h-[260px]">
             <CardHeader>
-              <CardTitle>Figma</CardTitle>
-              <CardDescription>Published by Ramsy</CardDescription>
+              <CardDescription>Nov. 27 2022</CardDescription>
+              <CardTitle className="flex items-center space-x-2">
+                <span className="w-fit h-[19px]">
+                  How is Web3 going to transform the dating industry
+                </span>
+                <div className="flex items-center space-x-1">
+                  <ChevronUp
+                    className="cursor-pointer"
+                    onClick={() => handleIncrement(setCount1)}
+                  />
+                  <span>{count1}</span>
+                  <ChevronDown
+                    className="cursor-pointer"
+                    onClick={() => handleDecrement(count1, setCount1)}
+                  />
+                </div>
+              </CardTitle>
+              <CardDescription className="pt-3">
+                Published by Ramsy
+              </CardDescription>
             </CardHeader>
-            <CardContent className="font-medium text-sm">
-              Figma is a collaborative web application for interface design,
-              with additional offline features enabled by desktop applications
-              for macOS and Windows.
+            <CardContent className="font-medium text-sm space-x-3 w-full">
+              <Button className="p-3 bg-black/25">Blockchain</Button>
+              <Button className="p-3  bg-black/25">Web3</Button>
+              <Button className="p-3  bg-black/25">Dating</Button>
+              <Button className="p-3  bg-black/25">Marketing</Button>
             </CardContent>
-            <CardFooter className="font-medium text-sm flex flex-wrap gap-2">
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                UI/UX Design
+            <CardFooter className="font-medium text-sm space-x-3">
+              <Button className="p-3 w-full h-11 py-4 px-[55px]  bg-black/50">
+                View
               </Button>
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                Web Design
-              </Button>
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                Mobile Design
+              <Button className="p-3 w-full h-11 py-4 px-[55px]">
+                Download
               </Button>
             </CardFooter>
           </Card>
-          <Card className="w-full h-full">
+          <Card className="w-full h-[260px]">
             <CardHeader>
-              <CardTitle>Figma</CardTitle>
-              <CardDescription>Published by Ramsy</CardDescription>
+              <CardDescription>Nov. 27 2022</CardDescription>
+              <CardTitle className="flex items-center space-x-2">
+                <span className="w-fit h-[19px]">
+                  How is Web3 going to transform the dating industry
+                </span>
+                <div className="flex items-center space-x-1">
+                  <ChevronUp
+                    className="cursor-pointer"
+                    onClick={() => handleIncrement(setCount1)}
+                  />
+                  <span>{count1}</span>
+                  <ChevronDown
+                    className="cursor-pointer"
+                    onClick={() => handleDecrement(count1, setCount1)}
+                  />
+                </div>
+              </CardTitle>
+              <CardDescription className="pt-3">
+                Published by Ramsy
+              </CardDescription>
             </CardHeader>
-            <CardContent className="font-medium text-sm">
-              Figma is a collaborative web application for interface design,
-              with additional offline features enabled by desktop applications
-              for macOS and Windows.
+            <CardContent className="font-medium text-sm space-x-3 w-full">
+              <Button className="p-3 bg-black/25">Blockchain</Button>
+              <Button className="p-3  bg-black/25">Web3</Button>
+              <Button className="p-3  bg-black/25">Dating</Button>
+              <Button className="p-3  bg-black/25">Marketing</Button>
             </CardContent>
-            <CardFooter className="font-medium text-sm flex flex-wrap gap-2">
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                UI/UX Design
+            <CardFooter className="font-medium text-sm space-x-3">
+              <Button className="p-3 w-full h-11 py-4 px-[55px]  bg-black/50">
+                View
               </Button>
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                Web Design
-              </Button>
-              <Button className="w-full sm:w-[97px] h-9 bg-black/50">
-                Mobile Design
+              <Button className="p-3 w-full h-11 py-4 px-[55px]">
+                Download
               </Button>
             </CardFooter>
           </Card>
+          
         </div>
       </div>
     </div>
